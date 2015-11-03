9f96cf0324dcd09e674118f472c538ed2cfce81e
Roberto Zazo Lopez <roberto.zazo.lopez@everis.com>
2015-11-03 12:42:56 +0100

    (test) function callback

--- src/proteo.ui.filter-contact-directive.js
+++ src/proteo.ui.filter-contact-directive.js
@@ -10,13 +10,13 @@ angular.module("proteo.ui.filter-contact", []).controller("filterContactCtrl",  
 	return {
 		//restrict: "AE",
 		scope: {
-			returnFilter : "=filter-callback"
+			filterCallback : "="
 		},
 		controller : 'filterContactCtrl',
 		templateUrl: './bower_components/proteo.ui.filter-contact/src/proteo.ui.filter-contact.tpl.html',
 		link: function($scope){
 			$scope.somethingHappend = function(){
-	       		$scope.returnFilter($scope.filter);
+	       		$scope.filterCallback($scope.filter);
 	 		}
 		}
 	}
