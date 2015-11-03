051f63915677a2344aeb56f605a4f02e14f6280b
Roberto Zazo Lopez <roberto.zazo.lopez@everis.com>
2015-11-03 10:58:35 +0100

    get out $element of controller [test]

--- src/proteo.ui.filter-contact-directive.js
+++ src/proteo.ui.filter-contact-directive.js
@@ -1,4 +1,4 @@
-angular.module("proteo.ui.filter-contact", []).controller("filterContactCtrl",  function($scope, $element) {
+angular.module("proteo.ui.filter-contact", []).controller("filterContactCtrl",  function($scope) {
 		console.log("that is of Controller of Filter");
 		
 		$scope.filter={};
